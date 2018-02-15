df40b9fbd952efa9d8d2fa85bf245ae4fc8827a0
gmkanik <35773774+gmkanik@users.noreply.github.com>
2018-02-15 11:24:38 -0500

    Update explanations.py

--- explanations.py
+++ explanations.py
@@ -1,21 +1,23 @@
-import tweepy
-import requests
-import datetime
-import pandas as pd
-import numpy as np
 import json
-import pycurl
+import tweepy
 
+#opens and read json file
 keys={}
 with open("json/keys.json") as file:
     keys = json.loads(file.read())
+    keys={}
     
 # Consumer keys and access tokens, used for OAuth
 consumer_key = keys["consumer_key"]
 consumer_secret = keys["consumer_secret"]
 access_token = keys["access_token"]
 access_token_secret = keys["access_token_secret"]
- 
-# OAuth process, using the keys and tokens
+
+#Twitter tweet
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+turtle = "turtle"
+api.update_status(turtle)
+
+print(turtle)
